e60091e6c1067a2cebd375436d0a718340f4b4df
Diab Neiroukh <officiallazerl0rd@gmail.com>
2020-04-08 21:55:44 +0100

    🔥

---
+++ README.md
@@ -0,0 +1,51 @@
+## Krayan GTK Theme
+
+Krayan is a flat Material Design theme for GTK3, GTK2, and GNOME Shell based on the well aquitted [Layan GTK theme](https://github.com/vinceliuice/Layan-gtk-theme) by [Kevin D'souza](https://github.com/krypticallusion) and [Diab Neiroukh](https://github.com/lazerl0rd). This is the KDE version based on [Layan's KDE theme](https://github.com/vinceliuice/layan-kde).It has a slightly darker twist on the colour scheme and prefers "Open Sans"-style fonts.
+
+### Info
+
+**This theme is not complete.** Only the Kvantum and Colour Scheme have been completed.
+
+### Info
+
+#### Kvantum
+
+This theme requires the Kvantum Engine.
+
+Arch:
+
+```
+pacman -S kvantummanager
+```
+
+Debian-based distros (Ubuntu, Mint, etc.):
+
+```
+sudo apt-get install kvantummanager
+```
+
+"Enterprise Linux"-based distros (Fedora, RedHat, etc.):
+
+```
+yum install kvantummanager
+```
+
+Others:
+
+Search for the engines in your distributions repository or install the engines from source.
+
+### Installation
+
+Arch (available at the AUR [here](https://aur.archlinux.org/packages/kvantum-theme-krayan-git) and [here](https://aur.archlinux.org/packages/krayan-kde-git)):
+
+```
+yay -S kvantum-theme-krayan-git krayan-kde-git
+```
+
+Others:
+
+Clone this repository, open the terminal at current directory, and then run `./install.sh`.
+
+### Removal
+
+Run `./uninstall.sh` script which will detect and remove Krayan.
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ROOT_UID=0
+DEST_DIR=
+
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  AURORAE_DIR="/usr/share/aurorae/themes"
+  SCHEMES_DIR="/usr/share/color-schemes"
+  PLASMA_DIR="/usr/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
+  KVANTUM_DIR="/usr/share/Kvantum"
+else
+  AURORAE_DIR="$HOME/.local/share/aurorae/themes"
+  SCHEMES_DIR="$HOME/.local/share/color-schemes"
+  PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+  KVANTUM_DIR="$HOME/.config/Kvantum"
+fi
+
+SRC_DIR=$(cd $(dirname $0) && pwd)
+
+THEME_NAME=Krayan
+
+install() {
+  local name=${1}
+
+  mkdir -p                                                                           ${AURORAE_DIR}
+  mkdir -p                                                                           ${SCHEMES_DIR}
+  mkdir -p                                                                           ${PLASMA_DIR}
+  mkdir -p                                                                           ${LOOKFEEL_DIR}
+  mkdir -p                                                                           ${KVANTUM_DIR}
+
+#  cp -r ${SRC_DIR}/aurorae/themes/*                                                  ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+#  cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/color-schemes/${name}.colors                                      ${PLASMA_DIR}/${name}/colors
+#  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
+}
+
+echo "Installing '${THEME_NAME} kde themes'..."
+
+install "${THEME_NAME}"
+
+echo "Install finished..."
